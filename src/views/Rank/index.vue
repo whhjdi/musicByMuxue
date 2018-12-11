a3ef7a09c7d4e1cdbca8d8629586c5d672bd08bf
whhjdi <418605872@qq.com>
2018-12-11 17:48:17 +0800

    解决接口idnex不对应的问题

--- src/views/Rank/index.vue
+++ src/views/Rank/index.vue
@@ -16,6 +16,32 @@
 </template>
 
 <script>
+const RANK_LIST = [
+  "云音乐新歌榜",
+  "云音乐热歌榜",
+  "网易原创歌曲榜",
+  "云音乐飙升榜",
+  "云音乐电音榜",
+  "UK排行榜周榜",
+  "美国Billboard周榜",
+  "KTV嗨榜",
+  "iTunes榜",
+  "Hit FM Top榜",
+  "日本Oricon周榜",
+  "韩国Melon排行榜周榜",
+  "韩国Mnet排行榜周榜",
+  "韩国Melon原声周榜",
+  "中国TOP排行榜(港台榜)",
+  "中国TOP排行榜(内地榜)",
+  "香港电台中文歌曲龙虎榜",
+  "华语金曲榜",
+  "中国嘻哈榜",
+  "法国 NRJ EuroHot 30周榜",
+  "台湾Hito排行榜",
+  "Beatport全球电子舞曲榜",
+  "云音乐ACG音乐榜",
+  "云音乐嘻哈榜"
+];
 import { createSong } from "@/utils";
 import Rank from "@/api/rank.js";
 import RankList from "@/components/Rank/RankList.vue";
@@ -57,7 +83,13 @@ export default {
       this.$router.push({
         path: `/rank/${item.id}`
       });
-      this.getTopListDetail(index);
+      let curIndex = 0;
+      for (let i = 0; i < RANK_LIST.length; i++) {
+        if (item.name === RANK_LIST[i]) {
+          curIndex = i;
+        }
+      }
+      this.getTopListDetail(curIndex);
     },
     getTopListDetail(idx) {
       Rank.getTopListDetail(idx).then(res => {
