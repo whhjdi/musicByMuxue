4dc860b75c7f43a2fadcb2550717d626cc9c4013
muxue <418605872@qq.com>
2019-12-04 14:21:46 +0800

    isProd use cdn min.js

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,21 @@
+const isProd = process.env.NODE_ENV === 'production';
 module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/musicByMuxue/dist/" : "/",
-  configureWebpack: {
-    externals: {
-      vue: "Vue",
-      "vue-router": "VueRouter",
-      vuex: "Vuex"
+  publicPath: isProd ? '/musicByMuxue/dist/' : '/',
+  configureWebpack: (config) => {
+    if (isProd) {
+      config.externals = {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        vuex: 'Vuex'
+      };
+    }
+  },
+  chainWebpack: (config) => {
+    if (isProd) {
+      config.plugin('html').tap((args) => {
+        args[0].cdn = true;
+        return args;
+      });
     }
   }
 };
